30e59d14bfcc8a19c96eabf8483a0d1bd1cb1f4e
maxivega0 <maximilianovega600@gmail.com>
2023-02-28 18:31:31 -0300

    Agregado de nuevo ejercicio sobre condicionales

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -11,5 +11,20 @@ else{
 // Solicitar la edad del usuario e indicar por pantalla si puede votar o no
 // Tengo que pedirle edad al usuario
 // puede votar a partir de los 16 años
+// 16 y 17 es opcional
+// A partir de 71 no es obligatorio
+// prompt palabra reservada para pedir un dato 
+// parseint('40')
 
-let edad = prompt('Ingrese su edad');
+// let edad = prompt('Ingrese su edad')
+let edad = parseInt(prompt('Ingrese su edad'));
+
+// edad = parseInt(edad)
+console.log(typeof(edad));
+console.log(edad);
+
+if(edad >= 16 && edad < 18 || edad >= 71){
+    document.write("Es optativo votar");
+}else{
+    document.write("Usted no puede ir a votar");
+}
