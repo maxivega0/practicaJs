20828d669500d24f54a475be17eeb37fd9073b98
maxivega0 <maximilianovega600@gmail.com>
2023-03-02 16:49:28 -0300

    ejercicio sobre edad corregido

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -13,18 +13,20 @@ else{
 // puede votar a partir de los 16 años
 // 16 y 17 es opcional
 // A partir de 71 no es obligatorio
-// prompt palabra reservada para pedir un dato 
+// prompt palabra reservada para pedir un dato
 // parseint('40')
 
 // let edad = prompt('Ingrese su edad')
-let edad = parseInt(prompt('Ingrese su edad'));
+let edad = parseInt(prompt("Ingrese su edad"));
 
 // edad = parseInt(edad)
-console.log(typeof(edad));
+console.log(typeof edad);
 console.log(edad);
 
-if(edad >= 16 && edad < 18 || edad >= 71){
-    document.write("Es optativo votar");
-}else{
-    document.write("Usted no puede ir a votar");
+if ((edad >= 16 && edad < 18) || edad >= 71) {
+  document.write("Es optativo ir a votar");
+} else if (edad >= 18 && edad <= 71) {
+  document.write("Es obligatorio ir a votar");
+} else {
+  document.write("Usted no puede ir a votar");
 }
