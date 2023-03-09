2d398d29e4e1f0d82c456aab6968e8c987725d92
maxivega0 <maximilianovega600@gmail.com>
2023-03-09 18:24:36 -0300

    dibujamos la tabla inicial para el usuario

--- js/ejercicios/ejercicio10-tp2.js
+++ js/ejercicios/ejercicio10-tp2.js
@@ -3,3 +3,19 @@
 Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente.
 Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 */
+
+const filas = prompt("ingrese el numero de filas");
+const columnas = prompt("ingrese el numero de columnas");
+
+console.log(filas)
+
+document.write("<table> <tbody>");
+for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
+    document.write(`
+    <tr>
+        <td>1</td>
+    </tr>
+`);
+}
+
+document.write("</tbody></table>");
