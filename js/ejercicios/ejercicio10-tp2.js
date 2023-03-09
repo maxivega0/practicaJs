564c9de2acbbda999ff7df79b58268faccdc4590
maxivega0 <maximilianovega600@gmail.com>
2023-03-09 20:09:48 -0300

    resolucion ej10 tp2

--- js/ejercicios/ejercicio10-tp2.js
+++ js/ejercicios/ejercicio10-tp2.js
@@ -1,21 +1,20 @@
-/*
-10- Realiza un script que pida número de filas y columnas y escriba una tabla. 
-Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente.
-Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
-*/
+/* 10- Realiza un script que pida número de filas y columnas y escriba una tabla.  Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1. */
 
 const filas = prompt("ingrese el numero de filas");
 const columnas = prompt("ingrese el numero de columnas");
-
+let numeracion = filas * columnas
 console.log(filas)
 
 document.write("<table> <tbody>");
+//* dibuja las filas
 for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
-    document.write(`
-    <tr>
-        <td>1</td>
-    </tr>
-`);
-}
+    document.write("<tr>")
+    //* dibuja las celdas
+    for(let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++){
+        document.write(`<td>${numeracion}</td>`);
+        numeracion--
+    }
 
+    document.write("</tr>")
+}
 document.write("</tbody></table>");
